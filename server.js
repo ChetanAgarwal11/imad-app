652ab744d64a5128722dd866123959de1518de1a
ChetanAgarwal11 <30628654+ChetanAgarwal11@users.noreply.github.com>
2017-08-14 15:29:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ var names = [];
 app.get('/submit-name/:name', function (req, res) {
    var name = req.params.name;
    
-   names.push[name];
+   names.push(name);
    
    res.send(JSON.stringify(names));
    
