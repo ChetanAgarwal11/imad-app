f10e76eb19b36ade6d47ca05e8bc553dcd522770
ChetanAgarwal11 <30628654+ChetanAgarwal11@users.noreply.github.com>
2017-08-14 13:48:48 +0530

    Create main.js

---
+++ ui/main.js
@@ -0,0 +1,42 @@
+// counter code 
+var button = document.getElementById('counter');
+
+button.onclick = function() {
+  
+  //Create a request object 
+  var request = new XMLHttpRequest();
+  
+  //capture the response and store it in a variable 
+  request.onreadystatechange = function () {
+    if(request.readyState === XMLHttpRequest.DONE){
+        //Take some action 
+        if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }  
+     //Not done yet
+  };
+  
+  //Make a request   
+  request.open('GET', 'http://chetan86866.imad.hasura-app.io/counter', true);
+  request.send(null);
+};
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit-btn');
+submit.onclick = function () {
+    //make req to the server 
+    
+    //capture a list of names and rendr them
+    var names = ['name1', 'name2', 'name3', 'name4' ];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list += '<li>'+names[i]+'</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
