6651e7c676aa7e544afc25e65bca23c2e8dfe2b2
ChetanAgarwal11 <30628654+ChetanAgarwal11@users.noreply.github.com>
2017-08-14 15:19:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ submit.onclick = function () {
     //make req to the server 
     
     //capture a list of names and rendr them
-    var names = ['name1', 'name2', 'name3', 'name4' ];
+    var names = ['name1', 'name2', 'name3', 'name4'];
     var list='';
     for(var i=0;i<names.length;i++){
         list += '<li>'+names[i]+'</li>';
