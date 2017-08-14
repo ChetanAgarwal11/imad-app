7a51675bb89d74010bb2f68e9f2ba8dc321094ae
ChetanAgarwal11 <30628654+ChetanAgarwal11@users.noreply.github.com>
2017-08-14 22:34:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'chetan86866',
+    database: 'chetan86866',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD 
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -64,6 +73,19 @@ function createTemplate(data){
 `;
     return htmlTemplate;
 }
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+   //connect to the db using node-postgres
+   //select command execution
+   pool.query('SELECT * FROM test', function (err, result) {
+       if(err){
+           res.status(500).send(err.toString());
+       }else {
+           res.send(JSON.stringify(result));
+       }
+   });
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
