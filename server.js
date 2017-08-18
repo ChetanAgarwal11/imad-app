5a5570062cfa704bbf9a811ea3a64a99b4d2d43a
ChetanAgarwal11 <30628654+ChetanAgarwal11@users.noreply.github.com>
2017-08-18 16:40:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.use(bodyParser.json());
 app.use(session({
     secret: 'someRandomSecretValue',
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30},
-    saveUninitialized: true
+    saveUninitialized: true,
+    resave: true
 }));
 
 function createTemplate (data) {
